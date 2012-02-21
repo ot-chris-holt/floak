8a05211d54d105a5d2650a23934f4a31d5bbc5a3
ot-chris-holt <cholt@technologies.net>
2012-02-21 00:49:58 -0600

    added a bunch of tests and features

--- floak/Program.fs
+++ floak/Program.fs
@@ -3,21 +3,99 @@
 open firefox
 open register
  
-describe "h1 should have John"
-url @"C:\projects\floak\floak\BasicPage.html"
-"#welcome" == "Welcome"
+let testpage = @"C:\projects\floak\floak\BasicPage.html"
 
-describe "textbox should have John"
-url @"C:\projects\floak\floak\BasicPage.html"
+describe "#welcome should have Welcome"
+url testpage
+read "#welcome" |> equals "Welcome"
+
+describe "#firstName should have John (using == infix operator)"
+url testpage
 "#firstName" == "John"
 
-describe "textbox should have Doe"
-url @"C:\projects\floak\floak\BasicPage.html"
+describe "#lastName should have Doe"
+url testpage
 "#lastName" == "Doe"
 
-describe "ajax label should have ajax loaded"
-url @"C:\projects\floak\floak\BasicPage.html"
+describe "clearing #firstName sets text to new empty string"
+url testpage
+clear "#firstName"
+"#firstName" == ""
+
+describe "writing to #lastName sets text to new Smith"
+url testpage
+clear "#lastName"
+write "#lastName" "Smith"
+"#lastName" == "Smith"
+
+describe "#ajax label should have ajax loaded"
+url testpage
 "#ajax" == "ajax loaded"
+
+describe "Value 1 listed in #value_list"
+url testpage
+listed "#value_list td" "Value 1"
+
+describe "Value 2 listed in #value_list"
+url testpage
+listed "#value_list td" "Value 2"
+
+describe "Value 3 listed in #value_list (using *= infix operator)"
+url testpage
+"#value_list td" *= "Value 3"
+
+describe "Value 4 listed in #value_list (using *= infix operator)"
+url testpage
+"#value_list td" *= "Value 4"
+
+describe "Item 1 listed in #item_list"
+url testpage
+listed "#item_list option" "Item 1"
+
+describe "Item 2 listed in #item_list"
+url testpage
+listed "#item_list option" "Item 2"
+
+describe "Item 3 listed in #item_list (using *= infix operator)"
+url testpage
+"#item_list option" *= "Item 3"
+
+describe "Item 4 listed in #item_list (using *= infix operator)"
+url testpage
+"#item_list option" *= "Item 4"
+
+describe "clicking #button sets #button_clicked to button clicked"
+url testpage
+"#button_clicked" == "button not clicked"
+click "#button"
+"#button_clicked" == "button clicked"
+
+describe "clicking hyperlink sets #link_clicked to link clicked"
+url testpage
+"#link_clicked" == "link not clicked"
+click "#hyperlink"
+"#link_clicked" == "link clicked"
+
+describe "clicking #radio1 selects it"
+url testpage
+click "#radio1"
+selected "#radio1"
+
+describe "clicking #radio2 selects it"
+url testpage
+click "#radio2"
+selected "#radio2"
+
+describe "clicking #checkbox selects it"
+url testpage
+click "#checkbox"
+selected "#checkbox"
+
+describe "clicking selected #checkbox deselects it"
+url testpage
+click "#checkbox"
+click "#checkbox"
+deselected "#checkbox"
 
 quit ()
 
--- floak/firefox.fs
+++ floak/firefox.fs
@@ -15,19 +15,45 @@ let write (cssSelector : string) (text : string) =
     element.SendKeys(text)
 
 let read (cssSelector : string) =    
-    let element = browser.FindElement(By.CssSelector(cssSelector))
-    if element.TagName = "input" then
-        element.GetAttribute("value")
-    else
-        element.Text    
+    try
+        let element = browser.FindElement(By.CssSelector(cssSelector))
+        if element.TagName = "input" then
+            element.GetAttribute("value")
+        else
+            element.Text    
+    with
+        | :? Exception -> "can't find element"
         
 let clear (cssSelector : string) = 
     let element = browser.FindElement(By.CssSelector(cssSelector))
     element.Clear()
     
 let click (cssSelector : string) = 
-    let element = browser.FindElement(By.CssSelector(cssSelector))
-    element.Click()
+    try
+        let element = browser.FindElement(By.CssSelector(cssSelector))
+        element.Click()
+    with
+        | :? Exception -> System.Console.WriteLine("cant find element {0}", cssSelector);
+
+let selected (cssSelector : string) = 
+    try
+        let element = browser.FindElement(By.CssSelector(cssSelector))
+        if element.Selected then
+            System.Console.WriteLine("element selected passed.");    
+        else
+            System.Console.WriteLine("element selected failed, {0} not selected.", cssSelector);    
+    with
+        | :? Exception -> System.Console.WriteLine("cant find element {0}", cssSelector);
+
+let deselected (cssSelector : string) = 
+    try
+        let element = browser.FindElement(By.CssSelector(cssSelector))
+        if element.Selected = false then
+            System.Console.WriteLine("element deselected passed.");    
+        else
+            System.Console.WriteLine("element deselected failed, {0} selected.", cssSelector);    
+    with
+        | :? Exception -> System.Console.WriteLine("cant find element {0}", cssSelector);
 
 let title _ = browser.Title
 
@@ -40,14 +66,24 @@ let equals value1 value2 =
         System.Console.WriteLine("equality check failed.  expected: {0}, got: {1}", value1, value2);
     ()
 
-let (==) element value =
+let ( == ) element value =
     let wait = new WebDriverWait(browser, TimeSpan.FromSeconds(3.0))
     try
-        wait.Until(fun b -> (read element) = value) |> ignore
+        wait.Until(fun _ -> (read element) = value) |> ignore
+        equals value (read element)
     with
-        | :? Exception -> ()
+        | :? Exception -> equals value "timeout trying to find element"
 
-    equals value (read element)
+let listed (cssSelector : string) value =
+    let wait = new WebDriverWait(browser, TimeSpan.FromSeconds(3.0))
+    try
+        wait.Until(fun _ -> browser.FindElements(By.CssSelector(cssSelector)) |> Seq.exists(fun element -> element.Text = value)) |> ignore
+        equals value value
+    with
+        | :? Exception -> equals value "timeout trying to find matching element in list"    
+
+let ( *= ) (cssSelector : string) value =
+    listed cssSelector value
     
 let contains (value1 : string) (value2 : string) =
     if (value2.Contains(value1) = true) then
